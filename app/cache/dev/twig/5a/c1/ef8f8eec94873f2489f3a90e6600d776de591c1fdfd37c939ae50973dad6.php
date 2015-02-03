42d2996a7e96aa40d2830d2ecff1d9f0e8609ab1
emmanueldd <e.derozin@gmail.com>
2015-02-03 03:28:41 +0100

    overrides the default template of FOS

--- app/cache/dev/twig/5a/c1/ef8f8eec94873f2489f3a90e6600d776de591c1fdfd37c939ae50973dad6.php
+++ app/cache/dev/twig/5a/c1/ef8f8eec94873f2489f3a90e6600d776de591c1fdfd37c939ae50973dad6.php
@@ -7,100 +7,49 @@ class __TwigTemplate_5ac1ef8f8eec94873f2489f3a90e6600d776de591c1fdfd37c939ae5097
     {
         parent::__construct($env);
 
-        $this->parent = false;
+        // line 1
+        try {
+            $this->parent = $this->env->loadTemplate("AcmeDemoBundle::layout.html.twig");
+        } catch (Twig_Error_Loader $e) {
+            $e->setTemplateFile($this->getTemplateName());
+            $e->setTemplateLine(1);
+
+            throw $e;
+        }
 
         $this->blocks = array(
+            'title' => array($this, 'block_title'),
+            'content' => array($this, 'block_content'),
             'fos_user_content' => array($this, 'block_fos_user_content'),
         );
     }
 
-    protected function doDisplay(array $context, array $blocks = array())
+    protected function doGetParent(array $context)
     {
-        // line 1
-        echo "<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset=\"UTF-8\" />
-    </head>
-    <body>
-        <div>
-            ";
-        // line 8
-        if ($this->env->getExtension('security')->isGranted("IS_AUTHENTICATED_REMEMBERED")) {
-            // line 9
-            echo "                ";
-            echo twig_escape_filter($this->env, $this->env->getExtension('translator')->trans("layout.logged_in_as", array("%username%" => $this->getAttribute($this->getAttribute((isset($context["app"]) ? $context["app"] : $this->getContext($context, "app")), "user", array()), "username", array())), "FOSUserBundle"), "html", null, true);
-            echo " |
-                <a href=\"";
-            // line 10
-            echo $this->env->getExtension('routing')->getPath("fos_user_security_logout");
-            echo "\">
-                    ";
-            // line 11
-            echo twig_escape_filter($this->env, $this->env->getExtension('translator')->trans("layout.logout", array(), "FOSUserBundle"), "html", null, true);
-            echo "
-                </a>
-            ";
-        } else {
-            // line 14
-            echo "                <a href=\"";
-            echo $this->env->getExtension('routing')->getPath("fos_user_security_login");
-            echo "\">";
-            echo twig_escape_filter($this->env, $this->env->getExtension('translator')->trans("layout.login", array(), "FOSUserBundle"), "html", null, true);
-            echo "</a>
-            ";
-        }
-        // line 16
-        echo "        </div>
-
-        ";
-        // line 18
-        $context['_parent'] = (array) $context;
-        $context['_seq'] = twig_ensure_traversable($this->getAttribute($this->getAttribute($this->getAttribute((isset($context["app"]) ? $context["app"] : $this->getContext($context, "app")), "session", array()), "flashbag", array()), "all", array(), "method"));
-        foreach ($context['_seq'] as $context["type"] => $context["messages"]) {
-            // line 19
-            echo "            ";
-            $context['_parent'] = (array) $context;
-            $context['_seq'] = twig_ensure_traversable($context["messages"]);
-            foreach ($context['_seq'] as $context["_key"] => $context["message"]) {
-                // line 20
-                echo "                <div class=\"flash-";
-                echo twig_escape_filter($this->env, $context["type"], "html", null, true);
-                echo "\">
-                    ";
-                // line 21
-                echo twig_escape_filter($this->env, $context["message"], "html", null, true);
-                echo "
-                </div>
-            ";
-            }
-            $_parent = $context['_parent'];
-            unset($context['_seq'], $context['_iterated'], $context['_key'], $context['message'], $context['_parent'], $context['loop']);
-            $context = array_intersect_key($context, $_parent) + $_parent;
-            // line 24
-            echo "        ";
-        }
-        $_parent = $context['_parent'];
-        unset($context['_seq'], $context['_iterated'], $context['type'], $context['messages'], $context['_parent'], $context['loop']);
-        $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 25
-        echo "
-        <div>
-            ";
-        // line 27
-        $this->displayBlock('fos_user_content', $context, $blocks);
-        // line 29
-        echo "        </div>
-    </body>
-</html>
-";
+        return "AcmeDemoBundle::layout.html.twig";
     }
 
-    // line 27
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_title($context, array $blocks = array())
+    {
+        echo "Acme Demo Application";
+    }
+
+    // line 5
+    public function block_content($context, array $blocks = array())
+    {
+        // line 6
+        echo "    ";
+        $this->displayBlock('fos_user_content', $context, $blocks);
+    }
+
     public function block_fos_user_content($context, array $blocks = array())
     {
-        // line 28
-        echo "            ";
     }
 
     public function getTemplateName()
@@ -115,6 +64,6 @@ class __TwigTemplate_5ac1ef8f8eec94873f2489f3a90e6600d776de591c1fdfd37c939ae5097
 
     public function getDebugInfo()
     {
-        return array (  103 => 28,  100 => 27,  93 => 29,  91 => 27,  87 => 25,  81 => 24,  72 => 21,  67 => 20,  62 => 19,  58 => 18,  54 => 16,  46 => 14,  40 => 11,  36 => 10,  31 => 9,  29 => 8,  20 => 1,);
+        return array (  47 => 6,  44 => 5,  38 => 3,  11 => 1,);
     }
 }
